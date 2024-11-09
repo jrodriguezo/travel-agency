62efcaf85ed8c60e6b205ee0c8c29167a7e15551
jrodriguezo <jrodriguezo.developer@gmail.com>
2024-11-09 01:40:12 +0100

    fix: test that modify network requests

--- tests/pw/filter-by-text.spec.ts
+++ tests/pw/filter-by-text.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from "@playwright/test";
+import { BASE_URL_VERSION, ENDPOINTS } from "../../src/config";
+import { travels } from "../../src/test/mockup";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:5173");
@@ -7,6 +9,13 @@ test.beforeEach(async ({ page }) => {
 test("Should display only one h2 with Portugal when input is filled", async ({
   page,
 }) => {
+  await page.route(`${BASE_URL_VERSION}${ENDPOINTS.TRAVELS}`, async (route) => {
+    await route.fulfill({
+      contentType: "application/json",
+      body: JSON.stringify(travels),
+    });
+  });
+
   await page.fill('input[placeholder="Search trips"]', "Portugal");
   await page.click('button:has-text("Search")');
 
