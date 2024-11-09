96c61c711df579d78136acfa4257e05f390c4b25
jrodriguezo <jrodriguezo.developer@gmail.com>
2024-11-09 19:22:36 +0100

    feat: add types

--- src/components/ui/travel-data/travel-data.type.ts
+++ src/components/ui/travel-data/travel-data.type.ts
@@ -1,16 +1,23 @@
+export type Id = number;
+export type Title = string;
+export type PhotoUrl = string;
+export type Description = string;
+export type Day = string;
+export type Location = string;
+
 export type ItineraryItem = {
-  day: number;
-  location: string;
-  description: string;
+  day: Day;
+  location: Location;
+  description: Description;
 };
 
 export type Status = "todo" | "done";
 
 export type Travel = {
-  id: number;
-  title: string;
-  photo_url: string;
-  description: string;
+  id: Id;
+  title: Title;
+  photo_url: PhotoUrl;
+  description: Description;
   status: Status;
   itinerary: ItineraryItem[];
 };
