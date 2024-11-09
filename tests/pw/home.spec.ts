ec72183340292a43853ceb815731d5fd615d8436
jrodriguezo <jrodriguezo.developer@gmail.com>
2024-11-09 01:14:11 +0100

    feat: use before each to avoid repetitive task on every test

--- tests/pw/home.spec.ts
+++ tests/pw/home.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:5173");
+});
+
+test("has title", async ({ page }) => {
   const title = await page.locator("h1");
   await expect(title).toHaveText("The places you dream of");
 });
 
 test("can create a new trip", async ({ page }) => {
-  await page.goto("http://localhost:5173");
   await page.getByRole("button", { name: "Create a new trip" }).click();
   const modalTitleLocator = page.getByRole("heading", {
     name: "Create a trip",
